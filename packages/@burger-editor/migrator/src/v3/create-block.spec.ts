89e49d5f1e3abc4e8c04b8ae5c1dd75566d14bb7
YusukeHirao <hirao@d-zero.co.jp>
2025-04-18 11:19:01 +0900

    feat(migrator): add v3 module with create-block functionality

--- packages/@burger-editor/migrator/src/v3/create-block.spec.ts
+++ packages/@burger-editor/migrator/src/v3/create-block.spec.ts
@@ -13,4 +13,52 @@ describe('createBlock', () => {
 	<div data-bgt="title-h2" data-bgt-ver="2.1.0" class="bgt-container bgt-title-h2-container"><h2 class="bge-title-h2" data-bge="title-h2">タイトル</h2></div>
 </div>`);
 	});
+
+	test('should create block with multiple items', () => {
+		const block = createBlock('imageText3', [
+			{
+				popup: false,
+				empty: 0,
+				hr: false,
+				path: '/path/to/img/sample1.jpg',
+				srcset: '',
+				alt: '画像1',
+				width: '',
+				height: '',
+				caption: '',
+			},
+			{
+				ckeditor: 'テキスト1\n**"サンプル1"**',
+			},
+			{
+				popup: false,
+				empty: 0,
+				hr: false,
+				path: '/path/to/img/sample2.jpg',
+				srcset: '',
+				alt: '画像2',
+				width: '',
+				height: '',
+				caption: '',
+			},
+			{
+				ckeditor: 'テキスト2\n**"サンプル2"**',
+			},
+			{
+				popup: false,
+				empty: 0,
+				hr: false,
+				path: '/path/to/img/sample3.jpg',
+				srcset: '',
+				alt: '画像3',
+				width: '',
+				height: '',
+				caption: '',
+			},
+			{
+				ckeditor: 'テキスト3\n**"サンプル3"**',
+			},
+		]);
+		expect(block).toMatchSnapshot();
+	});
 });
