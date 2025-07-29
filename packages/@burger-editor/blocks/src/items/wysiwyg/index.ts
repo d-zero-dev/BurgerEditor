c797c01f458357e3928a1551920179c8a761be8d
YusukeHirao <hirao@d-zero.co.jp>
2025-07-29 22:00:05 +0900

    feat(blocks): replace wysiwyg editor with custom element implementation

--- packages/@burger-editor/blocks/src/items/wysiwyg/index.ts
+++ packages/@burger-editor/blocks/src/items/wysiwyg/index.ts
@@ -1,6 +1,7 @@
+import type { BgeWysiwygEditorElement } from '@burger-editor/custom-element';
+
 import { createItem } from '../../create-item.js';
 
-import { createWysiwygEditor } from './create-wysiwyg-editor.js';
 import editor from './editor.html';
 import style from './style.css';
 import template from './template.html';
@@ -16,18 +17,7 @@ export default createItem<WysiwygData>({
 	style,
 	editor,
 	editorOptions: {
-		async open(data, editor) {
-			const PREVIEW_AREA_SELECTOR = '[data-bge-preview]';
-			const EDITOR_AREA_SELECTOR = '[data-bgi="wysiwyg"]';
-			const $editorArea = editor.find<HTMLDivElement>(EDITOR_AREA_SELECTOR);
-			if (!$editorArea) {
-				throw new Error(`Not found: "${EDITOR_AREA_SELECTOR}"`);
-			}
-
-			createWysiwygEditor($editorArea, editor, data.wysiwyg);
-
-			const style = document.createElement('style');
-
+		async open(_, editor) {
 			const css = await Promise.all(
 				editor.engine.css.stylesheets
 					.filter((sheet) => sheet.layer == null)
@@ -37,44 +27,8 @@ export default createItem<WysiwygData>({
 					}),
 			);
 
-			style.textContent = `
-				@scope (${PREVIEW_AREA_SELECTOR}) {
-					${css.join('\n')}
-				}
-
-				textarea[name="bge-wysiwyg"],
-				${PREVIEW_AREA_SELECTOR} ${EDITOR_AREA_SELECTOR} {
-					padding: 1em;
-					block-size: 50svh;
-					inline-size: 100%;
-					resize: vertical;
-					overflow-y: auto;
-				}
-
-				textarea[name="bge-wysiwyg"] {
-					font-family: var(--bge-font-family-monospace);
-				}
-
-				[data-bge-mode="wysiwyg"] textarea[name="bge-wysiwyg"] {
-					display: none;
-				}
-
-				[data-bge-mode="html"] [data-bge-preview] {
-					display: none;
-				}`;
-			editor.el.before(style);
-
-			const htmlModeButton = editor.find<HTMLButtonElement>(
-				'[data-bge-toolbar-button="html-mode"]',
-			)!;
-			htmlModeButton.addEventListener('click', () => {
-				htmlModeButton.ariaPressed =
-					htmlModeButton.ariaPressed === 'true' ? 'false' : 'true';
-				const mode = htmlModeButton.ariaPressed === 'true' ? 'html' : 'wysiwyg';
-				editor
-					.find<HTMLDivElement>(`[data-bge-mode]`)
-					?.setAttribute('data-bge-mode', mode);
-			});
+			const wysiwyg = editor.find<BgeWysiwygEditorElement>('bge-wysiwyg-editor');
+			wysiwyg?.setStyle(css.join('\n'));
 		},
 	},
 });
