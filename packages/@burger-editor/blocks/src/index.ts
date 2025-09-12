a5cd3a617ee1647d06922499f59e19e5e8a24d41
YusukeHirao <hirao@d-zero.co.jp>
2025-09-12 23:48:23 +0900

    feat(blocks): implement new data-driven block catalog system

--- packages/@burger-editor/blocks/src/index.ts
+++ packages/@burger-editor/blocks/src/index.ts
@@ -1,4 +1,5 @@
 export { items } from './items.js';
 export { defaultCatalog } from './default-catalog.js';
+export { oldCatalog } from './old-catalog.js';
 
 export { default as generalCSS } from './general.css';
--- packages/@burger-editor/blocks/src/old-catalog.ts
+++ packages/@burger-editor/blocks/src/old-catalog.ts
@@ -1,14 +1,24 @@
 import type { BlockCatalog } from '@burger-editor/core';
 
-// 全ブロックのアイコンをインポート
+import buttonIcon from './icons/button.svg';
+import button2Icon from './icons/button2.svg';
 import button3Icon from './icons/button3.svg';
 import contentNavigationIcon from './icons/content-navigation.svg';
 import disclosureIcon from './icons/disclosure.svg';
+import downloadFileIcon from './icons/download-file.svg';
+import downloadFile2Icon from './icons/download-file2.svg';
 import downloadFile3Icon from './icons/download-file3.svg';
 import googleMapsIcon from './icons/google-maps.svg';
 import hrIcon from './icons/hr.svg';
+import imageText2Icon from './icons/image-text2.svg';
 import imageText3Icon from './icons/image-text3.svg';
+import imageText4Icon from './icons/image-text4.svg';
+import imageText5Icon from './icons/image-text5.svg';
 import imageIcon from './icons/image.svg';
+import image2Icon from './icons/image2.svg';
+import image3Icon from './icons/image3.svg';
+import image4Icon from './icons/image4.svg';
+import image5Icon from './icons/image5.svg';
 import tableIcon from './icons/table.svg';
 import textFloatImage1Icon from './icons/text-float-image1.svg';
 import textFloatImage2Icon from './icons/text-float-image2.svg';
@@ -18,10 +28,11 @@ import textImage2Icon from './icons/text-image2.svg';
 import titleIcon from './icons/title.svg';
 import title2Icon from './icons/title2.svg';
 import wysiwygIcon from './icons/wysiwyg.svg';
+import wysiwyg2Icon from './icons/wysiwyg2.svg';
 import youtubeIcon from './icons/youtube.svg';
 
-export const defaultCatalog: BlockCatalog = {
-	見出し: [
+export const oldCatalog: BlockCatalog = {
+	'見出し / テキスト / テキスト+画像': [
 		{
 			label: '大見出し',
 			definition: {
@@ -44,10 +55,8 @@ export const defaultCatalog: BlockCatalog = {
 				items: [['title-h3']],
 			},
 		},
-	],
-	基本ブロック: [
 		{
-			label: 'テキスト',
+			label: '1カラムテキスト',
 			definition: {
 				name: 'wysiwyg',
 				svg: wysiwygIcon,
@@ -59,88 +68,17 @@ export const defaultCatalog: BlockCatalog = {
 			},
 		},
 		{
-			label: '画像',
+			label: '2カラムテキスト',
 			definition: {
-				name: 'image',
-				svg: imageIcon,
+				name: 'wysiwyg',
+				svg: wysiwyg2Icon,
 				containerProps: {
 					type: 'grid',
-					columns: 1,
+					columns: 2,
 				},
-				items: [['image']],
+				items: [['wysiwyg'], ['wysiwyg']],
 			},
 		},
-		{
-			label: '折りたたみ',
-			definition: {
-				name: 'disclosure',
-				svg: disclosureIcon,
-				containerProps: {
-					type: 'grid',
-					columns: 1,
-				},
-				items: [['details']],
-			},
-		},
-		{
-			label: 'テーブル',
-			definition: {
-				name: 'table',
-				svg: tableIcon,
-				containerProps: {
-					immutable: true,
-				},
-				items: [['table']],
-			},
-		},
-		{
-			label: 'YouTube',
-			definition: {
-				name: 'youtube',
-				svg: youtubeIcon,
-				containerProps: {
-					type: 'grid',
-					columns: 1,
-				},
-				items: [['youtube']],
-			},
-		},
-	],
-	カード: [
-		{
-			label: '画像 + テキスト',
-			definition: {
-				name: 'image-text',
-				svg: imageText3Icon,
-				containerProps: {
-					type: 'grid',
-					columns: 3,
-				},
-				items: [
-					['image', 'wysiwyg'],
-					['image', 'wysiwyg'],
-					['image', 'wysiwyg'],
-				],
-			},
-		},
-		{
-			label: 'テキスト+画像+テキスト',
-			definition: {
-				name: 'text-image-text',
-				svg: textImageTextIcon,
-				containerProps: {
-					type: 'grid',
-					columns: 3,
-				},
-				items: [
-					[{ name: 'wysiwyg', data: { wysiwyg: '<h2>見出し</h2>' } }, 'image', 'wysiwyg'],
-					[{ name: 'wysiwyg', data: { wysiwyg: '<h2>見出し</h2>' } }, 'image', 'wysiwyg'],
-					[{ name: 'wysiwyg', data: { wysiwyg: '<h2>見出し</h2>' } }, 'image', 'wysiwyg'],
-				],
-			},
-		},
-	],
-	'画像+テキスト': [
 		{
 			label: '画像右寄せ: テキスト回り込み',
 			definition: {
@@ -238,9 +176,181 @@ export const defaultCatalog: BlockCatalog = {
 			},
 		},
 	],
+	画像: [
+		{
+			label: '画像1列',
+			definition: {
+				name: 'image',
+				svg: imageIcon,
+				containerProps: {
+					type: 'grid',
+					columns: 1,
+				},
+				items: [['image']],
+			},
+		},
+		{
+			label: '画像2列',
+			definition: {
+				name: 'image',
+				svg: image2Icon,
+				containerProps: {
+					type: 'grid',
+					columns: 2,
+				},
+				items: [['image'], ['image']],
+			},
+		},
+		{
+			label: '画像3列',
+			definition: {
+				name: 'image',
+				svg: image3Icon,
+				containerProps: {
+					type: 'grid',
+					columns: 3,
+				},
+				items: [['image'], ['image'], ['image']],
+			},
+		},
+		{
+			label: '画像4列',
+			definition: {
+				name: 'image',
+				svg: image4Icon,
+				containerProps: {
+					type: 'grid',
+					columns: 4,
+				},
+				items: [['image'], ['image'], ['image'], ['image']],
+			},
+		},
+		{
+			label: '画像5列',
+			definition: {
+				name: 'image',
+				svg: image5Icon,
+				containerProps: {
+					type: 'grid',
+					columns: 5,
+				},
+				items: [['image'], ['image'], ['image'], ['image'], ['image']],
+			},
+		},
+	],
+	'画像+テキスト': [
+		{
+			label: '画像2列: テキスト付',
+			definition: {
+				name: 'image-text',
+				svg: imageText2Icon,
+				containerProps: {
+					type: 'grid',
+					columns: 2,
+				},
+				items: [
+					['image', 'wysiwyg'],
+					['image', 'wysiwyg'],
+				],
+			},
+		},
+		{
+			label: '画像3列: テキスト付',
+			definition: {
+				name: 'image-text',
+				svg: imageText3Icon,
+				containerProps: {
+					type: 'grid',
+					columns: 3,
+				},
+				items: [
+					['image', 'wysiwyg'],
+					['image', 'wysiwyg'],
+					['image', 'wysiwyg'],
+				],
+			},
+		},
+		{
+			label: '画像4列: テキスト付',
+			definition: {
+				name: 'image-text4',
+				svg: imageText4Icon,
+				containerProps: {
+					type: 'grid',
+					columns: 4,
+				},
+				items: [
+					['image', 'wysiwyg'],
+					['image', 'wysiwyg'],
+					['image', 'wysiwyg'],
+					['image', 'wysiwyg'],
+				],
+			},
+		},
+		{
+			label: '画像5列: テキスト付',
+			definition: {
+				name: 'image-text',
+				svg: imageText5Icon,
+				containerProps: {
+					type: 'grid',
+					columns: 5,
+				},
+				items: [
+					['image', 'wysiwyg'],
+					['image', 'wysiwyg'],
+					['image', 'wysiwyg'],
+					['image', 'wysiwyg'],
+					['image', 'wysiwyg'],
+				],
+			},
+		},
+	],
+	カード: [
+		{
+			label: 'テキスト+画像+テキスト',
+			definition: {
+				name: 'text-image-text',
+				svg: textImageTextIcon,
+				containerProps: {
+					type: 'grid',
+					columns: 3,
+				},
+				items: [
+					[{ name: 'wysiwyg', data: { wysiwyg: '<h3>見出し</h3>' } }, 'image', 'wysiwyg'],
+					[{ name: 'wysiwyg', data: { wysiwyg: '<h3>見出し</h3>' } }, 'image', 'wysiwyg'],
+					[{ name: 'wysiwyg', data: { wysiwyg: '<h3>見出し</h3>' } }, 'image', 'wysiwyg'],
+				],
+			},
+		},
+	],
 	ボタン: [
 		{
 			label: 'ボタン',
+			definition: {
+				name: 'button',
+				svg: buttonIcon,
+				containerProps: {
+					justify: 'center',
+					wrap: 'wrap',
+				},
+				items: [['button']],
+			},
+		},
+		{
+			label: 'ボタン x2',
+			definition: {
+				name: 'button',
+				svg: button2Icon,
+				containerProps: {
+					justify: 'center',
+					wrap: 'wrap',
+				},
+				items: [['button'], ['button']],
+			},
+		},
+		{
+			label: 'ボタン x3',
 			definition: {
 				name: 'button',
 				svg: button3Icon,
@@ -252,39 +362,31 @@ export const defaultCatalog: BlockCatalog = {
 			},
 		},
 		{
-			label: 'テキストリンク',
+			label: 'ファイルダウンロード',
 			definition: {
-				name: 'button',
-				svg: button3Icon,
+				name: 'file',
+				svg: downloadFileIcon,
 				containerProps: {
-					frameSemantics: 'ul',
-					justify: 'start',
+					justify: 'center',
 					wrap: 'wrap',
 				},
-				items: [
-					[
-						{
-							name: 'button',
-							data: { kind: 'link', text: 'テキストリンク' },
-						},
-					],
-					[
-						{
-							name: 'button',
-							data: { kind: 'link', text: 'テキストリンク' },
-						},
-					],
-					[
-						{
-							name: 'button',
-							data: { kind: 'link', text: 'テキストリンク' },
-						},
-					],
-				],
+				items: [['download-file']],
 			},
 		},
 		{
-			label: 'ファイルダウンロード',
+			label: 'ファイルダウンロード x2',
+			definition: {
+				name: 'file',
+				svg: downloadFile2Icon,
+				containerProps: {
+					justify: 'center',
+					wrap: 'wrap',
+				},
+				items: [['download-file'], ['download-file']],
+			},
+		},
+		{
+			label: 'ファイルダウンロード x3',
 			definition: {
 				name: 'file',
 				svg: downloadFile3Icon,
@@ -295,13 +397,14 @@ export const defaultCatalog: BlockCatalog = {
 				items: [['download-file'], ['download-file'], ['download-file']],
 			},
 		},
+	],
+	ナビゲーション: [
 		{
 			label: 'コンテンツナビゲーション',
 			definition: {
 				name: 'content-navigation',
 				svg: contentNavigationIcon,
 				containerProps: {
-					frameSemantics: 'ul',
 					type: 'grid',
 					columns: 4,
 				},
@@ -339,6 +442,12 @@ export const defaultCatalog: BlockCatalog = {
 					[
 						{
 							name: 'button',
+							data: { kind: 'in-page', text: 'コンテンツ5', link: '#content5' },
+						},
+					],
+					[
+						{
+							name: 'button',
 							data: { kind: 'in-page', text: 'コンテンツ6', link: '#content6' },
 						},
 					],
@@ -360,6 +469,17 @@ export const defaultCatalog: BlockCatalog = {
 	],
 	その他: [
 		{
+			label: '2カラムテーブル',
+			definition: {
+				name: 'table',
+				svg: tableIcon,
+				containerProps: {
+					immutable: true,
+				},
+				items: [['table']],
+			},
+		},
+		{
 			label: 'Google Maps',
 			definition: {
 				name: 'google-maps',
@@ -372,6 +492,18 @@ export const defaultCatalog: BlockCatalog = {
 			},
 		},
 		{
+			label: 'YouTube',
+			definition: {
+				name: 'youtube',
+				svg: youtubeIcon,
+				containerProps: {
+					type: 'grid',
+					columns: 1,
+				},
+				items: [['youtube']],
+			},
+		},
+		{
 			label: '区切り線',
 			definition: {
 				name: 'hr',
@@ -380,6 +512,18 @@ export const defaultCatalog: BlockCatalog = {
 					immutable: true,
 				},
 				items: [['hr']],
+			},
+		},
+		{
+			label: '折りたたみ',
+			definition: {
+				name: 'disclosure',
+				svg: disclosureIcon,
+				containerProps: {
+					type: 'grid',
+					columns: 1,
+				},
+				items: [['details']],
 			},
 		},
 	],
