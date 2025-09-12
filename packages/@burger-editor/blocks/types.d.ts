6bd0b0e8098f28c7f40eaebd6097ca86b5377b89
YusukeHirao <hirao@d-zero.co.jp>
2025-09-12 23:48:23 +0900

    feat(blocks): implement new data-driven block catalog system

--- packages/@burger-editor/blocks/types.d.ts
+++ packages/@burger-editor/blocks/types.d.ts
@@ -1,5 +1,7 @@
-import type { ItemSeed } from '@burger-editor/core';
+import type { BlockCatalog, ItemSeed } from '@burger-editor/core';
 
+declare const defaultCatalog: BlockCatalog;
+declare const oldCatalog: BlockCatalog;
 declare const items: Record<string, ItemSeed>;
 
 declare const generalCSS: string;
