7b0abff80e01bfcff53bdbd0d1073c915943d7ed
Yusuke Hirao <hirao@d-zero.co.jp>
2025-10-22 17:00:43 +0900

    test(blocks): add XSS sanitization tests for wysiwyg item
    Add test cases to verify XSS sanitization behavior:
    
    - Script tags are sanitized by default
    
    - dangerouslySetInnerHTML flag allows raw HTML when needed

--- packages/@burger-editor/blocks/src/items/wysiwyg/index.spec.ts
+++ packages/@burger-editor/blocks/src/items/wysiwyg/index.spec.ts
@@ -43,3 +43,80 @@ test('render', async () => {
 		'</div>',
 	]);
 });
+
+test('Safely set innerHTML', async () => {
+	const el = await render(
+		{
+			name: 'text',
+			containerProps: {
+				type: 'grid',
+				columns: 2,
+			},
+			items: [
+				[
+					{
+						name: 'wysiwyg',
+						data: {
+							wysiwyg: '<script>alert("XSS")</script>',
+						},
+					},
+				],
+			],
+		},
+		{ items },
+	);
+
+	expect(el.outerHTML.split(/(?<=>)\s*(?=<)/)).toEqual([
+		'<div data-bge-name="text" data-bge-container="grid:2">',
+		'<div data-bge-container-frame="">',
+		'<div data-bge-group="">',
+		'<div data-bge-item="">',
+		`<div data-bgi="wysiwyg" data-bgi-ver="${__VERSION__}">`,
+		'<div data-bge="wysiwyg">alert("XSS")</div>',
+		'</div>',
+		'</div>',
+		'</div>',
+		'</div>',
+		'</div>',
+	]);
+});
+
+test('dangerouslySetInnerHTML', async () => {
+	const el = await render(
+		{
+			name: 'text',
+			containerProps: {
+				type: 'grid',
+				columns: 2,
+			},
+			items: [
+				[
+					{
+						name: 'wysiwyg',
+						data: {
+							wysiwyg: '<script>alert("XSS")</script>',
+							dangerouslySetInnerHTML: true,
+						},
+					},
+				],
+			],
+		},
+		{ items },
+	);
+
+	expect(el.outerHTML.split(/(?<=>)\s*(?=<)/)).toEqual([
+		'<div data-bge-name="text" data-bge-container="grid:2">',
+		'<div data-bge-container-frame="">',
+		'<div data-bge-group="">',
+		'<div data-bge-item="">',
+		`<div data-bgi="wysiwyg" data-bgi-ver="${__VERSION__}">`,
+		'<div data-bge="wysiwyg">',
+		'<script>alert("XSS")</script>',
+		'</div>',
+		'</div>',
+		'</div>',
+		'</div>',
+		'</div>',
+		'</div>',
+	]);
+});
