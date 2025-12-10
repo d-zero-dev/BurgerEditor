ca1ad19c1a54ddcb5db4f63e852984f16030c7db
Yusuke Hirao <hirao@d-zero.co.jp>
2025-12-10 23:18:19 +0900

    feat(utils): add normalizeHtmlStructure function
    Add utility function to compare HTML structures ignoring whitespace and attribute order.

--- packages/@burger-editor/utils/src/index.ts
+++ packages/@burger-editor/utils/src/index.ts
@@ -2,5 +2,6 @@ export * from './camel-case.js';
 export * from './kebab-case.js';
 export * from './markdown.js';
 export * from './merge-items.js';
+export * from './normalize-html.js';
 export * from './replace-comment-with-html.js';
 export * from './utils.js';
